3c880064965ffc3355c1181710b516e40af8aef1
antoniotex <acm.skt@gmail.com>
2018-04-14 01:09:11 -0300

    Feito marcação JSX

---
+++ src/app.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const App = () => (
+    <div className='app'>
+        <div className='search'>
+            <input type='search' placeholder='Digite o nome do usuário' />
+            <button>Buscar</button>
+        </div>
+
+        <div className='user-info'>
+            <img src='https://avatars2.githubusercontent.com/u/26071112?v=4' alt='foto'/>
+            <h1><a href='https://github.com/antoniotex'>AntonioTx</a></h1>
+            <ul className='repos-info'>
+                <li>- Repositórios: 122</li>
+                <li>- Seguidores: 10</li>
+                <li>- Segundo: 20</li>
+            </ul>
+            <div className='actions'>
+                <button>Ver Repositórios</button>
+                <button>Ver Favoritos</button>
+            </div>
+            <div className='repos'>
+                <h2>Repositórios</h2>
+                <ul>
+                    <li><a href='#'>Nome do Repositório</a></li>
+                </ul>
+            </div>
+            <div className='starred'>
+                <h2>Favoritos</h2>
+                <ul>
+                    <li><a href='#'>Nome do Repositório</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+)
+
+export default App
